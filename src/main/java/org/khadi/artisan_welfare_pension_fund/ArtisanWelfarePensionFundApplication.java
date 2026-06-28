221fbd8481ab6cc083327f62743116b951e1e7a0
nirakar-asha <nirakarasha@gmail.com>
2026-06-28 17:55:31 +0530

    first commit

---
+++ src/main/java/org/khadi/artisan_welfare_pension_fund/ArtisanWelfarePensionFundApplication.java
@@ -0,0 +1,13 @@
+package org.khadi.artisan_welfare_pension_fund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtisanWelfarePensionFundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtisanWelfarePensionFundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/khadi/artisan_welfare_pension_fund/ArtisanWelfarePensionFundApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khadi.artisan_welfare_pension_fund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtisanWelfarePensionFundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
